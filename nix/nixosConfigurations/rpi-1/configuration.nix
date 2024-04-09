3eff083057d4e49adfd878f39cc18e3480b2da97
Steffen Vogel <post@steffenvogel.de>
2024-04-09 15:33:30 +0200

    nix: Allow cross-building SD-card image
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- nix/nixosConfigurations/rpi-1/configuration.nix
+++ nix/nixosConfigurations/rpi-1/configuration.nix
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2024 Steffen Vogel, OPAL-RT Germany GmbH
 # SPDX-License-Identifier: Apache-2.0
-{self, ...}: {
+{self, lib, ...}: {
   imports = [
     self.nixosModules.rpi-base
     self.nixosModules.gateway
@@ -12,6 +12,10 @@
   in ''
     cp ${defaultGatewayConfig} ./files/boot
   '';
+
+  nixpkgs = {
+    buildPlatform = lib.systems.examples.aarch64-multiplatform;
+  };
 
   system.stateVersion = "23.11";
 }
