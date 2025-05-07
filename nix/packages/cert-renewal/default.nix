c05870917d3200ff1f6d6584a89100fb9f866aa4
Steffen Vogel <post@steffenvogel.de>
2025-05-07 16:49:24 +0200

    feat(pki): Add EST certificate renewal script
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- nix/packages/cert-renewal/default.nix
+++ nix/packages/cert-renewal/default.nix
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025 Steffen Vogel, OPAL-RT Germany GmbH
+# SPDX-License-Identifier: Apache-2.0
+
 {
   writeShellApplication,
   curl,
@@ -10,5 +13,5 @@ writeShellApplication {
     curl
     openssl
   ];
-  text = builtins.readFile ./cert-renewal.sh;
+  text = builtins.readFile ./renewal.sh;
 }
