e890c246e5ddc357bcdae5cfe9190662634d7ea0
Felix Wege <fwege@eonerc.rwth-aachen.de>
2025-10-09 14:45:28 +0200

    fix(subscription-handler): remove unused import
    Signed-off-by: Felix Wege <fwege@eonerc.rwth-aachen.de>

--- seguro/gateway/opc_ua/subscription_handler.py
+++ seguro/gateway/opc_ua/subscription_handler.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
 # SPDX-License-Identifier: Apache-2.0
 import asyncio
-import time
 
 from enum import Enum
 from asyncua import Client
